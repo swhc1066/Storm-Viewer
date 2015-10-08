88e3dce4ccc9796bcd884602c052a40edf4d0731
Sean Craig <fwcycling@gmail.com>
2015-10-08 12:56:04 -0500

    Final project

---
+++ Hacking Swift P1/DetailViewController.swift
@@ -0,0 +1,55 @@
+//
+//  DetailViewController.swift
+//  Hacking Swift P1
+//
+//  Created by Sean Craig on 10/7/15.
+//  Copyright © 2015 Sean Craig. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+
+    @IBOutlet weak var detailImageView: UIImageView!
+
+
+    var detailItem: String? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let imageView   = self.detailImageView {
+                imageView.image = UIImage(named: detail)
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+
+}
+
